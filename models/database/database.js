a2e7931c43244ea4fec69c1943865a46945a78ae
ankul07 <96623109+ankul07@users.noreply.github.com>
2022-10-03 15:15:55 +0100

    Update database.js

--- models/database/database.js
+++ models/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config()
 
-mongoose.connect("mongodb+srv://ankur:ankur@cluster0.xtyv4.mongodb.net/ankulfood?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+mongoose.connect("mongodb+srv://<username>:<password>@cluster0.xtyv4.mongodb.net/ankulfood?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
     console.log("successful connected");
 }).catch((err)=>{
     console.log("not connection");
